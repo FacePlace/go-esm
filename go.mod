ce86710f8f9e6cee423cbdeea7e0621ec4b851ed
ZeroWorldwide <43105109+FacePlace@users.noreply.github.com>
2022-04-08 20:39:58 +0300

    Flatbuffers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/FacePlace/go-esm
 
 go 1.18
+
+require github.com/google/flatbuffers v2.0.6+incompatible
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"github.com/google/flatbuffers/go"
 	"math"
 )
 
@@ -13,6 +14,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%v\n", binary.LittleEndian.Uint32(buffer[4:8]))
+	fmt.Printf("%v\n", flatbuffers.GetUint8(buffer[0:1]))
+	fmt.Printf("%v\n", flatbuffers.GetUint32(buffer[4:8]))
 	fmt.Printf("%v", math.Float32frombits(binary.LittleEndian.Uint32(buffer[4:8])))
 }
